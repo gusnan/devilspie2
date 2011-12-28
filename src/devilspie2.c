28ea01662f1b777d0209b753bb16b5d0764102f2
Andreas Rönnquist <gusnan@gusnan.se>
2011-12-28 21:13:05 +0100

    Remove bind_result variable

--- src/devilspie2.c
+++ src/devilspie2.c
@@ -231,8 +231,7 @@ int main(int argc, char *argv[])
 	
 	gchar *locale_folder=g_build_path(G_DIR_SEPARATOR_S,DEVILSPIE2_PREFIX,"share/locale/",NULL);
 	
-	gchar *bind_result=NULL;
-	bind_result=bindtextdomain(GETTEXT_PACKAGE_NAME,locale_folder);
+	bindtextdomain(GETTEXT_PACKAGE_NAME,locale_folder);
 	bind_textdomain_codeset(GETTEXT_PACKAGE_NAME,"");
 	textdomain(GETTEXT_PACKAGE_NAME);
 	
